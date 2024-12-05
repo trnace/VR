56305929121d245ac2284e0cd0cbe2e103a54f4a
trnace <ray0636enen@gmail.com>
2024-12-05 11:10:21 +0800

    teach

---
+++ teach/data.js
@@ -0,0 +1,292 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "台北大學",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0061304619394242366,
+          "pitch": 0.174482782923997,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.4077158277750357,
+          "pitch": 0.07278206889202643,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.5629662275035443,
+          "pitch": 0.07343531953849691,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5825721710530454,
+          "pitch": -0.04884952869513981,
+          "title": "<span id=\"docs-internal-guid-9bc51241-7fff-0f89-39b2-a47d268a8894\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">國立臺北大學</span></span>",
+          "text": "<span id=\"docs-internal-guid-2491207c-7fff-7fd7-2f47-6cbd97156f85\"><p dir=\"ltr\" style=\"line-height:1.38;margin-top:0pt;margin-bottom:0pt;\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">國立臺北大學，簡稱北大，前身為1949年創立的「臺灣省立地方行政專科學校」、1955年改制的「臺灣省立法商學院」以及1961年整併成立</span><a href=\"https://zh.wikipedia.org/wiki/%E8%87%BA%E7%81%A3%E7%9C%81%E7%AB%8B%E4%B8%AD%E8%88%88%E5%A4%A7%E5%AD%B8\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; color: rgb(255, 255, 255); background-color: transparent; font-weight: 400; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">臺灣省立中興大學</span></a><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">的法商學院（</span><a href=\"https://zh.wikipedia.org/wiki/%E5%9C%8B%E7%AB%8B%E4%B8%AD%E8%88%88%E5%A4%A7%E5%AD%B8%E6%B3%95%E5%95%86%E5%AD%B8%E9%99%A2\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; color: rgb(255, 255, 255); background-color: transparent; font-weight: 400; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">中興法商</span></a><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">）與臺北進修部</span><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">，2000年時脫離</span><a href=\"https://zh.wikipedia.org/wiki/%E5%9C%8B%E7%AB%8B%E4%B8%AD%E8%88%88%E5%A4%A7%E5%AD%B8\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; color: rgb(255, 255, 255); background-color: transparent; font-weight: 400; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">國立中興大學</span></a><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">獨立設校，並改為現名。</span></p></span>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "北大高中",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07823771897731824,
+          "pitch": 0.005469592904994158,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.20940358466888576,
+          "pitch": -0.0013168847939208916,
+          "rotation": 1.5707963267948966,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8671773853555749,
+          "pitch": -0.04429533927928375,
+          "title": "北大高中",
+          "text": "<span id=\"docs-internal-guid-d79e4946-7fff-c0c4-4fdc-848172a933ff\"><p dir=\"ltr\" style=\"line-height:1.38;margin-top:0pt;margin-bottom:0pt;\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">新北市立北大高級中學，簡稱北大高中，是新北市三峽區北大特區的一所市立完全中學，隸屬於新北市政府。</span></p><div><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\"><br></span></div></span>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "桃子腳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09999960093179894,
+          "pitch": 0.01974294532633536,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.0751144102859822,
+          "pitch": 0.015196088365216909,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5614216720800114,
+          "pitch": -0.3138385749000534,
+          "title": "<span style=\"font-family: Arial, sans-serif; font-size: 14.6667px; white-space-collapse: preserve; background-color: rgba(58, 68, 84, 0.8);\">桃子腳</span>",
+          "text": "<span id=\"docs-internal-guid-935b99b1-7fff-953b-140a-0483f560897d\"><p dir=\"ltr\" style=\"line-height:1.38;margin-top:0pt;margin-bottom:0pt;\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">新北市立桃子腳國民中小學是一所位於台灣新北市樹林區的國中國小學校。校地面績有5.2公頃，設有國小部48班與國中部21班。校內種植有數十棵茄苳、九芎、玉蘭及楓香等老樹，原本種在樂生療養院用地，但因該院當時成為捷運新莊機廠的預定地，樹可能被砍除，於2002年間移植至桃子腳。</span></p><div><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\"><br></span></div></span>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "教育研究院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3034501690815041,
+          "pitch": 0.3156203398507422,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -3.1083718344911766,
+          "pitch": 0.19033517823267232,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8483099340654228,
+          "pitch": -0.004638998219615331,
+          "title": "教育研究院",
+          "text": "<span id=\"docs-internal-guid-65ee30c4-7fff-3039-fbdc-409100a91eec\"><p dir=\"ltr\" style=\"line-height:1.38;margin-top:0pt;margin-bottom:0pt;\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">國家教育研究院，簡稱國教院，是中華民國關於教育研究工作的最高專責機構，隸屬教育部。國教院除了進行整體性、系統性、長期性的教育研究以外，也負有教科書審定、課程研發、原住民族教育及測驗評量及辦理中小學行政人員研習等任務</span></p><div><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\"><br></span></div></span>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "籠埔國小",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2253240699361001,
+          "pitch": -0.005868095949443486,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.9931154491592746,
+          "pitch": 0.0428100482376621,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9519792259211748,
+          "pitch": -0.2299749083743432,
+          "title": "龍埔國小",
+          "text": "<span id=\"docs-internal-guid-2d426c26-7fff-3ba7-edf7-8d5abfa4cf96\"><p dir=\"ltr\" style=\"line-height:1.38;margin-top:0pt;margin-bottom:0pt;\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">2012學年度經新北市政府核定為新生額滿學校及雙語實驗課程學校，並委託協辦新北市候用校長儲訓、英語教育、專業社群、電子書業務。</span></p><div><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\"><br></span></div></span>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
