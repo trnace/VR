d24def75b15be64d2a0547011364dc8dfe669696
trnace <ray0636enen@gmail.com>
2024-12-05 11:57:22 +0800

    play

---
+++ play/data.js
@@ -0,0 +1,304 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "麥當勞",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.4977755976857665,
+        "pitch": -0.1616577615130943,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5782810946620263,
+          "pitch": 0.05017684441691017,
+          "rotation": 0,
+          "target": "3-andand"
+        },
+        {
+          "yaw": -1.696990415641105,
+          "pitch": 0.01505889907544855,
+          "rotation": 11.780972450961727,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8450250186370667,
+          "pitch": -0.4189280445155461,
+          "title": "麥當勞北大店",
+          "text": "曾經歷過2次裝修"
+        },
+        {
+          "yaw": -0.5328055718546238,
+          "pitch": -0.32229889863826067,
+          "title": "多合一餐廳",
+          "text": "樓上曾開過各種餐廳"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "街區內側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31950464925807687,
+          "pitch": 0.0717169520777663,
+          "rotation": 0,
+          "target": "3-andand"
+        },
+        {
+          "yaw": -1.2544189721601224,
+          "pitch": 0.04595039626192765,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.450690835511537,
+          "pitch": 0.18751172229800162,
+          "title": "裝置藝術",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "樹林三峽分界",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15862301824812874,
+          "pitch": -0.008559138690172219,
+          "rotation": 1.5707963267948966,
+          "target": "3-andand"
+        },
+        {
+          "yaw": 1.5926354761607149,
+          "pitch": 0.07388859759275945,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2927748107625767,
+          "pitch": 0.06473722838400775,
+          "title": "大義路",
+          "text": "左側為樹林 右側為三峽"
+        }
+      ]
+    },
+    {
+      "id": "3-andand",
+      "name": "公園and購物大樓and圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1032705469517676,
+          "pitch": 0.03139306672382958,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.025364141889165026,
+          "pitch": 0.04572482788668353,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1614637918351427,
+          "pitch": -0.26327578997804935,
+          "title": "購物樓&amp;餐廳",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.6260075028429748,
+          "pitch": 0.017128445009765514,
+          "title": "公園",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.8540844453569036,
+          "pitch": -0.12751470792390762,
+          "title": "圖書館&amp;警察局",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "家樂福",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.078319388033437,
+          "pitch": -0.01054358339645134,
+          "rotation": 1.5707963267948966,
+          "target": "3-andand"
+        },
+        {
+          "yaw": -0.04915292883059763,
+          "pitch": 0.02710043930360051,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.357557571676683,
+          "pitch": -0.329372491944774,
+          "title": "家樂福北大店",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
